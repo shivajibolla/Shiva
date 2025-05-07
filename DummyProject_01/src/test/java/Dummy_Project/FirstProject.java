35843038cbec91c1fafd87ff2c83c55666b2c7df
shiva <shiva@AnSh>
2025-05-07 13:10:21 +0530

    FirstCommitUpdated

---
+++ DummyProject_01/src/test/java/Dummy_Project/FirstProject.java
@@ -0,0 +1,13 @@
+package Dummy_Project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstProject {
+
+	@Test
+	public void addDummy() {
+		Reporter.log("Dummy test is executed",true);
+		
+	}
+}
